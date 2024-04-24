223fde540707b489771073dbfdf77f23c853b53b
CemalShabinas <cemalshabi@gmail.com>
2024-04-24 10:26:03 +0400

    update

---
+++ src/script.js
@@ -0,0 +1,5 @@
+// Check if the URL contains "/exclusivesclub"
+if (window.location.href.includes("/exclusivesclub")) {
+  // Redirect to the desired URL for the Exclusives Club Shop
+  window.location.href = "https://cemalshabinas.com/exclusivesclub";
+}
